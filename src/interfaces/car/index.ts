ee6ec49129f5af54b554f74fe62773686c01a3dc
roq-ai <>
2023-07-11 12:53:15 +0000

    Initial commit

---
+++ src/interfaces/car/index.ts
@@ -0,0 +1,25 @@
+import { BookingInterface } from 'interfaces/booking';
+import { CompanyInterface } from 'interfaces/company';
+import { GetQueryInterface } from 'interfaces';
+
+export interface CarInterface {
+  id?: string;
+  model: string;
+  location: string;
+  availability?: boolean;
+  company_id?: string;
+  created_at?: any;
+  updated_at?: any;
+  booking?: BookingInterface[];
+  company?: CompanyInterface;
+  _count?: {
+    booking?: number;
+  };
+}
+
+export interface CarGetQueryInterface extends GetQueryInterface {
+  id?: string;
+  model?: string;
+  location?: string;
+  company_id?: string;
+}
---
+++ src/validationSchema/cars/index.ts
@@ -0,0 +1,8 @@
+import * as yup from 'yup';
+
+export const carValidationSchema = yup.object().shape({
+  model: yup.string().required(),
+  location: yup.string().required(),
+  availability: yup.boolean().required(),
+  company_id: yup.string().nullable(),
+});
